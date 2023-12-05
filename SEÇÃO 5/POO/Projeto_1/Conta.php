28605ee0b56ad8e171dbcabf65494a7712203a42
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-12-05 15:44:51 -0300

    Modificação 05/12/2023 15:44 Trabalho

--- SEÇÃO 5/POO/Projeto_1/Conta.php
+++ SEÇÃO 5/POO/Projeto_1/Conta.php
@@ -1,8 +1,46 @@
 <?php
-
-
 class Conta
 {
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public string $telefoneTitular;
+    public float $saldoTitular;
+
+
+    public function sacar($valor)
+    {
+        if ($valor > $this->saldoTitular)
+        {
+            echo "Saldo para saque insuficiente!" . PHP_EOL;
+        }
+        else if ($valor == $this->saldoTitular)
+        {
+            $this->saldoTitular = 0;
+
+        }
+        else
+        {
+            $this->saldoTitular -= $valor;
+
+        }
+
+
+    }
+
+    public function transferir(float $valor, Conta $contaDestino): void
+    {
+        if ($valor > $this->saldoTitular)
+        {
+            echo "Valor de transição insuficiente!" . PHP_EOL;
+
+        }
+
+        $this->saldoTitular -= $valor;
+        $contaDestino->saldoTitular += $valor;
+
+
+    }
+
 
 }
 
---
+++ SEÇÃO 5/POO/Projeto_1/main.php
@@ -0,0 +1,39 @@
+<?php
+
+
+include("Conta.php");
+
+$teste = new Conta();
+$teste2 = new Conta();
+
+$teste->nomeTitular = "Juan Guilherme Silva Lemos";
+$teste->cpfTitular = "10488775400";
+$teste->telefoneTitular = "(81)996272911";
+$teste->saldoTitular = 1000;
+
+
+// $teste->sacar(2900);
+
+
+
+
+
+$teste2->nomeTitular = "jonas";
+$teste2->cpfTitular = "1624888755500";
+$teste2->telefoneTitular = "(81)966271411";
+$teste2->saldoTitular = 1000;
+
+
+$teste->transferir(200, $teste2);
+
+
+
+var_dump($teste);
+var_dump($teste2);
+
+
+
+
+
+
+?>
